d6cc290e7e790fbb54c46825a3b2344755a36044
Mehmet Kudsi Odaman <whitechavomocha@gmail.com>
2021-12-26 22:21:01 +0300

    Last Stable Build
    Game's core mechanic moved another dimension :+1:

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Ball
+{
+    public class Ball : MonoBehaviour
+    {
+        private Rigidbody2D rb2d;
+
+        // Use this for initialization
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rb2d.velocity == Vector2.zero)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
--- Assets/Scripts/Ball/BallLineRenderer.cs
+++ Assets/Scripts/Ball/BallLineRenderer.cs
@@ -1,22 +1,22 @@
-using UnityEngine;
+//using UnityEngine;
 
-namespace Ball
-{
-    public class BallLineRenderer : MonoBehaviour
-    {
-        [SerializeField] private LineRenderer lineRenderer;
-        private Transform pivot;
+//namespace Ball
+//{
+//    public class BallLineRenderer : MonoBehaviour
+//    {
+//        [SerializeField] private LineRenderer lineRenderer;
+//        private Transform pivot;
 
-        private void Start()
-        {
-            pivot = FindObjectOfType<Pivot>().GetComponent<Transform>();
-        }
+//        private void Start()
+//        {
+//            pivot = FindObjectOfType<Pivot>().GetComponent<Transform>();
+//        }
 
-        // Update is called once per frame
-        private void Update()
-        {
-            lineRenderer.SetPosition(0, pivot.transform.position);
-            lineRenderer.SetPosition(1, this.transform.position);
-        }
-    }
-}
+//        // Update is called once per frame
+//        private void Update()
+//        {
+//            lineRenderer.SetPosition(0, pivot.transform.position);
+//            lineRenderer.SetPosition(1, this.transform.position);
+//        }
+//    }
+//}
